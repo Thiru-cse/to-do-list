24a9a1928c16f0b4b3e8b4eeb4a3fe1df5b634c0
Thiru-cse <thiru.c.cse@gmail.com>
2021-10-30 23:06:53 -0400

    To-Do-Lists🎉

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 const About = () => {
   return (
     <div>
-      <h4>Version 1.0.0</h4>
+        <h2 style={{color:'blue'}}>Created By Thiru.Cse😎</h2>
+      <h5>Using React Router Dom & JSON</h5>
       <Link to='/'>Go Back</Link>
     </div>
   )
